d88979b2c7a751b75dfc3ee0bb7dac8945bcf779
CabeludoCego <lukkasshock_@outlook.com>
2025-07-07 18:22:56 -0300

    ✨Expenses: Update, Delete and Archive operations

--- expenses/urls.py
+++ expenses/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
 	path('', views.index, name="expenses.index"),
  	path('create/', views.create, name='expenses.create'),
  	path('create-form/', views.create_form, name='expenses.create-form'),
+	path('update/<int:id>', views.update, name='expenses.update'),
+ 	path('archive/<int:id>', views.archive, name='expenses.archive'),
+ 	path('delete/<int:id>', views.delete, name='expenses.delete'),
+ 	
+  
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,4 @@
+from operator import is_
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
@@ -9,8 +10,13 @@ from django.contrib import messages
 @login_required(login_url="/auth/login")
 def index(request):
     user = request.user
-    expenses = Expense.objects.filter(owner = user)
-    return render(request, 'expenses/index.html', {'expenses':expenses});
+    expenses = Expense.objects.filter(owner = user, is_active=True)
+    categories = Category.objects.all()
+    context = {
+        'expenses':expenses,
+        'categories': categories
+    }
+    return render(request, 'expenses/index.html', context);
 
 @login_required(login_url="/auth/login")
 def create(request):
@@ -46,4 +52,74 @@ def create_form(request):
     else:
         form = ExpenseCreateModelForm()
     return render(request, 'expenses/create-djForm.html', {'form': form})
+
+@login_required(login_url="/auth/login")
+def update(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        'expense': expense,
+        'categories': categories,
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/update.html', context);
+    if request.method == 'POST':
+        amount = request.POST['amount'] 
+        description = request.POST['description']
+        category = request.POST['category']
+        date = request.POST['date']
+        
+        # METHOD 1: ONE by one
+        # expense.amount = amount
+        # expense.date = date
+        # expense.category = category
+        # expense.description = description
+        # expense.save()
+        
+        # METHOD 2: Update or create method
+        Expense.objects.update_or_create( pk=id, defaults={
+            "amount": amount,"description": description, "date":date, "category":category})
+        messages.success(request, 'Expense updated successfully!')
+        
+        return redirect('expenses.index')
     
+
+@login_required(login_url="/auth/login")
+def archive(request, id):    
+    expense = Expense.objects.get(pk=id)
+    expense.is_active = False
+    expense.save()
+    messages.success(request, 'Expense archived!')
+    return redirect('expenses.index')
+    
+    
+@login_required(login_url="/auth/login")
+def delete(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, "Expense removed!")
+    return redirect('expenses.index')
+
+
+# @login_required(login_url="/auth/login")
+# def update_form(request, id):
+#     expense = Expense.objects.get(pk=id)
+#     categories = Category.objects.all()
+#     context = {
+#         'expense': expense,
+#         'categories': categories,
+#     }
+#     # import pdb; pdb.set_trace()
+#     if request.method == 'GET':
+#         return render(request, 'expenses/update.html', context);
+#     if request.method == 'POST':
+#         amount = request.POST['amount'] 
+#         description = request.POST['description']
+#         category = request.POST['category']
+#         date = request.POST['date']
+        
+#         Expense.objects.update_or_create( pk=id, defaults={
+#             "amount": amount,"description": description, "date":date, "category":category})
+#         messages.success(request, 'Expense updated successfully!')
+        
+#         return redirect('expenses.index')
